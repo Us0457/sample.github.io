f8e2f525c4fc1e645140b341ab0e232f737a07c7
utkarsh Srivastava <utkarsh5679077@gmail.com>
2022-07-18 10:32:30 +0530

    Second commit

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 class A{
 	public static void main (String args[]){
-		System.out.println("Hello Class");
+		System.out.println("Hello Class A");
+	}
+}
+
+class B{
+	public static void main (String args[]){
+		System.out.println("Hello Class B");
 	}
 }
